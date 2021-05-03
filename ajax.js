89daa755b292066bd56d608689c3277b18f42ec9
LukasDrsman <lukaskodr@gmail.com>
2021-05-03 11:50:24 +0200

    Remove unnecessary whitespaces

--- ajax.js
+++ ajax.js
@@ -2,19 +2,19 @@ const toAjax = () => {
 	let xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = () => {};
 	let locs = "";
-	for( let i = 0; i < locations.length; i++ ) {
+	for(let i = 0; i < locations.length; i++) {
 		locs += locations[ i ].name;
-		if( (i + 1) != locations.length ) {
+		if((i + 1) != locations.length) {
 			locs += " - ";
 		}
 	}
-	xmlhttp.open( "GET", "php/addR.php?c=" + encodeURIComponent(locs) + "&km=" + distance.toString(), true );
+	xmlhttp.open("GET", "php/addR.php?c=" + encodeURIComponent( locs ) + "&km=" + distance.toString(), true);
 	xmlhttp.send();
 }
 
-document.getElementById( "send" ).addEventListener("click", () => {
-	if( window.navigator.onLine ) {
-		toAjax( locations, distance );
+document.getElementById("send").addEventListener("click", () => {
+	if(window.navigator.onLine) {
+		toAjax(locations, distance);
 		scanner.stop();
 		document.cookie = "path=";
 	}
--- handle-qr.js
+++ handle-qr.js
@@ -6,29 +6,29 @@ let locations = [];
 let rendered = 0;
 let calculated = 1;
 
-const lookup = ( loc1, loc2 ) => {
+const lookup = (loc1, loc2) => {
 	let dist;
-	if( loc1.id in distances ) {
-		if( loc2.id in distances[ loc1.id ] ) {
-			return distances[ loc1.id ][ loc2.id ];
+	if(loc1.id in distances) {
+		if(loc2.id in distances[loc1.id]) {
+			return distances[loc1.id][loc2.id];
 		}
-	} else if( loc2.id in distances ) {
-		if( loc1.id in distances[ loc2.id ] ) {
-			return distances[ loc2.id ][ loc1.id ];
+	} else if(loc2.id in distances) {
+		if(loc1.id in distances[loc2.id]) {
+			return distances[loc2.id][loc1.id];
 		}
 	}
 };
 
 const getCookie = name => {
 	name = name + "=";
-	let ca = document.cookie.split( ';' );
-	for( let i = 0; i < ca.length; i++ ) {
-		let c = ca[ i ];
-		while( c.charAt( 0 ) == ' ' ) {
-			c = c.substring( 1 );
+	let ca = document.cookie.split(";");
+	for(let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while(c.charAt(0) == " ") {
+			c = c.substring(1);
 		}
-		if( c.indexOf( name ) == 0 ) {
-			return c.substring( name.length, c.length );
+		if(c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
 		}
 	}
 	return "";
@@ -38,58 +38,58 @@ const updateCookie = locations => {
 	let loc = {
 		path: locations
 	};
-	document.cookie = "path=" + JSON.stringify( loc );
+	document.cookie = "path=" + JSON.stringify(loc);
 };
 
 const fillFromCookie = cookie => {
-	let locsObj = JSON.parse( cookie );
+	let locsObj = JSON.parse(cookie);
 	locations = locsObj.path;
 };
 
 const renderLocations = () => {
-	for( let i = rendered; i < locations.length; i++ ) {
-		let x = document.createElement( "TR" );
-		let y = document.createElement( "TD" );
+	for(let i = rendered; i < locations.length; i++) {
+		let x = document.createElement("TR");
+		let y = document.createElement("TD");
 		//x.setAttribute( "class", "table-primary" );
 		//y.setAttribute( "class", "table-primary" );
-		y.innerHTML = locations[ i ].name;
-		x.appendChild( y );
-		document.getElementById( "path" ).appendChild( x );
+		y.innerHTML = locations[i].name;
+		x.appendChild(y);
+		document.getElementById("path").appendChild(x);
 	}
 	rendered = locations.length;
 };
 
 const computeDistance = () => {
-	for( let i = calculated; i < locations.length; i++ ) {
-		distance += lookup( locations[ i ], locations[ i - 1 ] );
+	for(let i = calculated; i < locations.length; i++) {
+		distance += lookup(locations[i], locations[i - 1]);
 	}
-	document.getElementById( "distance" ).innerHTML = distance.toString() + "km";
+	document.getElementById("distance").innerHTML = distance.toString() + "km";
 	calculated = locations.length;
 };
 
 const scanner = new QrScanner(
-	document.getElementById( "videoElem" ),
+	document.getElementById("videoElem"),
 	result => {
-		let location = JSON.parse( result );
-		if( location.id != previous.id ) {
-			locations.push( location );
+		let location = JSON.parse(result);
+		if(location.id != previous.id) {
+			locations.push(location);
 			previous = location;
-			console.log( locations );
-			updateCookie( locations );
+			console.log(locations);
+			updateCookie(locations);
 			renderLocations();
 			computeDistance();
 		}
 	}
 );
 
-let path = getCookie( "path" );
-console.log( path );
+let path = getCookie("path");
+console.log(path);
 
 if( path == "{[]}" || path == "" ) {
 	scanner.start();
 } else {
-	fillFromCookie( path );
-	previous = locations[ locations.length - 1 ];
+	fillFromCookie(path);
+	previous = locations[locations.length - 1];
 	renderLocations();
 	computeDistance();
 	scanner.start();
