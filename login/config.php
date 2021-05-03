226148378ff820fa4e777f4de3b4956757fc429f
Peter Iglalrčík <83549235+Peter-Iglarcik@users.noreply.github.com>
2021-05-03 03:00:53 +0200

    Update config.php

--- login/config.php
+++ login/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'a043um.forpsi.com');
-define('DB_USERNAME', 'xxxxxx);
-define('DB_PASSWORD', 'xxxxxx);
+define('DB_USERNAME', 'xxxxxx');
+define('DB_PASSWORD', 'xxxxxx');
 define('DB_NAME', 'f147316');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
