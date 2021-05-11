7759643e1328376e15e1e62ee804c67cd78cbe3e
LukasDrsman <lukaskodr@gmail.com>
2021-05-11 22:31:40 +0200

    Remove window.onload

--- veduci/js/table.js
+++ veduci/js/table.js
@@ -4,9 +4,7 @@ const update = setInterval( () => {
 	table.load("php/table.php");
 }, 5000);
 
-window.onload = () => {
-	setTimeout( () => {
-		clearInterval();
-		update();
-	}, 240000);
-};
+setTimeout( () => {
+	clearInterval();
+	update();
+}, 240000);
